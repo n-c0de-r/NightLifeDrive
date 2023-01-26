868c2c325f4061d9dc156c783cd91b2fc972e493
n-c0de-r <78597469+n-c0de-r@users.noreply.github.com>
2023-01-26 22:59:17 +0100

    Make the name plate text change
    There is now an input field in the pause menu to set the name plate on the car.

--- NightLifeDrive/Assets/Scripts/Game.cs
+++ NightLifeDrive/Assets/Scripts/Game.cs
@@ -2,24 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Game : MonoBehaviour
 {
     public static Health health = new Health();
+
     [SerializeField] private AudioSource endSoundEffect;
+    
+    [SerializeField]
+    private TMP_InputField inputName;
+
+    [SerializeField]
+    private TextMeshPro namePlate;
+
     // Start is called before the first frame update
     void Start()
     {   
         //this.health = health.gameObject.AddComponent<Health>();
         health.setHealth(3);
+
+        inputName.text = namePlate.text;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(health.getHealth()==0){
-            StartCoroutine(End());  
+            StartCoroutine(End());
         }
+
+        namePlate.text = inputName.text;
     }
 
     private IEnumerator End(){
